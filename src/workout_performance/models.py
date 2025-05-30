9fbeeee1b1c821b86f191fbbecc8240faab29234
FRATAK4 <fratak4@gmail.com>
2025-05-30 23:30:57 +0200

    feat(defining_models): defines ExerciseSetPerformance model

--- src/workout_performance/models.py
+++ src/workout_performance/models.py
@@ -1,3 +1,14 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+from training_plans.models import ExerciseSet
+
+
+class ExerciseSetPerformance(models.Model):
+    weight = models.DecimalField(decimal_places=1)
+    repetitions_done = models.IntegerField()
+    estimated_max = models.DecimalField(decimal_places=1)
+    bodyweight = models.DecimalField(decimal_places=1)
+    date = models.DateTimeField(default=datetime.now())
+    exercise_set = models.ForeignKey(ExerciseSet, on_delete=models.CASCADE)
