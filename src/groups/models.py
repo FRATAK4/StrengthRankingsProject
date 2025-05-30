b3b6ca19312aff6cdca5e6356aa493133ca79023
FRATAK4 <fratak4@gmail.com>
2025-05-30 22:27:21 +0200

    feat(defining_models): defines Group model and adds group_pics directory

--- src/groups/models.py
+++ src/groups/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Group(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=2000)
+    image = models.ImageField(default='group_pics/default.jpg', upload_to='group_pics')
+    members = models.ManyToManyField(User)
+    join_requests = models.ManyToManyField(User)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
