54788fd3db655b42ddaef8d1ed37a79ef004d8d8
FRATAK4 <fratak4@gmail.com>
2025-05-30 22:50:09 +0200

    feat(defining_models): defines Workout model

--- src/training_plans/models.py
+++ src/training_plans/models.py
@@ -6,3 +6,18 @@ class TrainingPlan(models.Model):
     description = models.CharField(max_length=2000)
     is_active = models.BooleanField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Workout(models.Model):
+    class Days(models.TextChoices):
+        MONDAY = 'monday'
+        TUESDAY = 'tuesday'
+        WEDNESDAY = 'wednesday'
+        THURSDAY = 'thursday'
+        FRIDAY = 'friday'
+        SATURDAY = 'saturday'
+        SUNDAY = 'sunday'
+
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=2000)
+    day = models.CharField(max_length=30, choices=Days.choices)
+    training_plan = models.ForeignKey(TrainingPlan, on_delete=models.CASCADE)
