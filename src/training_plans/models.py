de6ad117026912b0b02865662b4adfc22d9c64de
FRATAK4 <fratak4@gmail.com>
2025-05-30 22:42:42 +0200

    feat(defining_models): defines TrainingPlan model

--- src/training_plans/models.py
+++ src/training_plans/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class TrainingPlan(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=2000)
+    is_active = models.BooleanField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
