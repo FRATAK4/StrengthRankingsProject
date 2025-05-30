1745fa82f7e562d543dd71aa039210d050ef7fa5
FRATAK4 <fratak4@gmail.com>
2025-05-30 22:01:46 +0200

    feat(defining_models): defines Profile model and adds media directory

--- src/account/models.py
+++ src/account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    class Genders(models.TextChoices):
+        MALE = 'male'
+        FEMALE = 'female'
+
+    nick = models.CharField(max_length=50)
+    image = models.ImageField(default='profile_pics/default.jpg', upload_to='profile_pics')
+    gender = models.CharField(max_length=30, choices=Genders.choices)
+    age = models.IntegerField()
+    bodyweight = models.DecimalField(decimal_places=1)
