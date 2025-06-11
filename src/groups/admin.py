34504a169b9f8469796d6b101977f73b5f318512
FRATAK4 <fratak4@gmail.com>
2025-06-11 18:59:19 +0200

    feat: registers new models in admin panel

--- src/groups/admin.py
+++ src/groups/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Group, GroupAddRequest, GroupMembership
+
+admin.site.register(Group)
+admin.site.register(GroupAddRequest)
+admin.site.register(GroupMembership)
--- src/training_plans/admin.py
+++ src/training_plans/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import TrainingPlan, Workout, ExerciseSet
+from .models import TrainingPlan, Workout, ExerciseSet, TrainingPlanUsage, TrainingPlanRating
 
 admin.site.register(TrainingPlan)
 admin.site.register(Workout)
 admin.site.register(ExerciseSet)
+admin.site.register(TrainingPlanUsage)
+admin.site.register(TrainingPlanRating)
