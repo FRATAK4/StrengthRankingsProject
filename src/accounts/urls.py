9cdd9f0713abde33ff574612c6859b13ab065c8b
FRATAK4 <fratak4@gmail.com>
2025-06-13 16:25:58 +0200

    feat(register): base of register functionality

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
 
+from .views import UserCreateView
+
 urlpatterns = [
     path(
         "login/",
         LoginView.as_view(template_name="accounts/login.html"),
         name="accounts-login",
-    )
+    ),
+    path("register/", UserCreateView.as_view(), name="accounts-register"),
 ]
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
 
-# Create your views here.
+from .forms import UserCreateForm
+
+
+class UserCreateView(CreateView):
+    form_class = UserCreateForm
+    template_name = "accounts/register.html"
