52a3398a7085208b1b660e776b48261ec69a960e
FRATAK4 <fratak4@gmail.com>
2025-06-16 00:14:18 +0200

    feat(core_base): url in core

--- src/core/urls.py
+++ src/core/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("analytics/", include("analytics.urls")),
+    path("home/", include("common.urls")),
 ]
