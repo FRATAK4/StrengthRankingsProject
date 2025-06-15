93ba2312bf7993fe79c51dd041303d7301f5d87e
FRATAK4 <fratak4@gmail.com>
2025-06-15 23:37:56 +0200

    feat(analytics_base): url in core

--- src/analytics/urls.py
+++ src/analytics/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 
 from .views import UserStatsView
 
-urlpatterns = [path("analytics/<int:pk>/", UserStatsView.as_view(), name="analytics")]
+urlpatterns = [path("<int:pk>/", UserStatsView.as_view(), name="analytics")]
