cb7b785f83af36e2adf2d07e38e05ccf1a39c43b
FRATAK4 <fratak4@gmail.com>
2025-06-19 03:31:59 +0200

    feat(training_plans_base): fills url base

--- src/training_plans/urls.py
+++ src/training_plans/urls.py
@@ -1,8 +1,37 @@
 from django.urls import path
 
-from src.training_plans.views import UserTrainingPlansListView, TrainingPlansListView
+from src.training_plans.views import (
+    UserTrainingPlanListView,
+    TrainingPlanListView,
+    TrainingPlanDetailView,
+    TrainingPlanCreateView,
+    TrainingPlanRatingListView,
+    TrainingPlanRatingDetailView,
+    TrainingPlanRatingCreateView,
+    WorkoutDetailView,
+)
 
 urlpatterns = [
-    path("", TrainingPlansListView.as_view(), name="training_plans"),
-    path("user/", UserTrainingPlansListView.as_view(), name="training_plans-user"),
+    path("", TrainingPlanListView.as_view(), name="training_plans"),
+    path("user/", UserTrainingPlanListView.as_view(), name="training_plans-user"),
+    path("<int:pk>/", TrainingPlanDetailView.as_view(), name="training_plan"),
+    path(
+        "workouts/<int:pk>", WorkoutDetailView.as_view(), name="training_plan-workout"
+    ),
+    path("create/", TrainingPlanCreateView.as_view(), name="training_plan-create"),
+    path(
+        "<int:pk>/ratings/",
+        TrainingPlanRatingListView.as_view(),
+        name="training_plan-ratings",
+    ),
+    path(
+        "<int:pk>/ratings/<int:pk_rating>/",
+        TrainingPlanRatingDetailView.as_view(),
+        name="training_plan-rating",
+    ),
+    path(
+        "<int:pk>/ratings/create/",
+        TrainingPlanRatingCreateView.as_view(),
+        name="training_plan-rating_create",
+    ),
 ]
