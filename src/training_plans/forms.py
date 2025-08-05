2d79cb9ba2f05ff650f02cf421fb811da52fa0ff
unknown <fratak4@gmail.com>
2025-08-05 07:53:31 +0200

    feat(training_plans_app): adds forms to forms.py

---
+++ src/training_plans/forms.py
@@ -0,0 +1,51 @@
+from django.forms.models import ModelForm, inlineformset_factory
+from .models import TrainingPlan, Workout, WorkoutExercise, ExerciseSet
+
+
+class TrainingPlanForm(ModelForm):
+    class Meta:
+        model = TrainingPlan
+        fields = ["name", "description"]
+
+class WorkoutForm(ModelForm):
+    class Meta:
+        model = Workout
+        fields = ["name", "description"]
+
+class WorkoutExerciseForm(ModelForm):
+    class Meta:
+        model = WorkoutExercise
+        fields = ["exercise"]
+
+class ExerciseSetForm(ModelForm):
+    class Meta:
+        model = ExerciseSet
+        fields = ["repetitions"]
+
+WorkoutFormSet = inlineformset_factory(
+    parent_model=Workout,
+    model=WorkoutExercise,
+    form=WorkoutExerciseForm,
+    extra=0,
+    can_delete=True,
+    min_num=1,
+    validate_min=True,
+    max_num=15,
+    validate_max=True,
+)
+
+WorkoutExerciseFormSet = inlineformset_factory(
+    parent_model=WorkoutExercise,
+    model=ExerciseSet,
+    form=ExerciseSetForm,
+    extra=0,
+    can_delete=True,
+    min_num=1,
+    validate_min=True,
+    max_num=50,
+    validate_max=True,
+)
+
+
+
+
