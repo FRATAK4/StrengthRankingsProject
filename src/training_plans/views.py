4123102ebc104bca7e2475fcc9ee66c3c2b2f0d7
unknown <fratak4@gmail.com>
2025-08-05 08:14:59 +0200

    feat(training_plans_app): adds empty views to views.py

---
+++ src/training_plans/views.py
@@ -0,0 +1,31 @@
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+
+
+class TrainingPlanListView(ListView):
+    pass
+
+class TrainingPlanCreateView(CreateView):
+    pass
+
+class TrainingPlanDetailView(DetailView):
+    pass
+
+class TrainingPlanUpdateView(UpdateView):
+    pass
+
+class TrainingPlanDeleteView(DeleteView):
+    pass
+
+class WorkoutsFormView(UpdateView):
+    pass
+
+class ExercisesFormView(UpdateView):
+    pass
+
+class WorkoutDetailView(DetailView):
+    pass
+
+class WorkoutUpdateView(UpdateView):
+    pass
+
+
