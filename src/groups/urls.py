49cd7be01cb3e6406c5404517ea237622abbf139
FRATAK4 <fratak4@gmail.com>
2025-08-13 22:36:28 +0200

    feat(groups app): clearing files in app directory

--- src/groups/urls.py
+++ src/groups/urls.py
@@ -1,19 +1,4 @@
 from django.urls import path
 
-from .views import (
-    GroupListView,
-    UserGroupListView,
-    GroupRequestListView,
-    GroupDetailView,
-    GroupRankingDetailView,
-    GroupCreateView,
-)
 
-urlpatterns = [
-    path("", GroupListView.as_view(), name="groups"),
-    path("user/", UserGroupListView.as_view(), name="groups-user"),
-    path("create/", GroupCreateView.as_view(), name="groups-create"),
-    path("requests/", GroupRequestListView.as_view(), name="groups-requests"),
-    path("<int:pk>/", GroupDetailView.as_view(), name="group"),
-    path("<int:pk>/rankings/", GroupRankingDetailView.as_view(), name="group-ranking"),
-]
+urlpatterns = []
