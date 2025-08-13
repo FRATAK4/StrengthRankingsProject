8997fc3f767fcdc525881aac30422f2322dafb36
FRATAK4 <fratak4@gmail.com>
2025-08-13 22:51:07 +0200

    feat(groups app): creates base of empty views classes

--- src/groups/views.py
+++ src/groups/views.py
@@ -1,1 +1,40 @@
+from django.views.generic import (
+    View,
+    ListView,
+    CreateView,
+    DetailView,
+    UpdateView,
+    DeleteView,
+)
 
+
+class GroupDashboardView(View):
+    pass
+
+
+class OwnedGroupListView(ListView):
+    pass
+
+
+class JoinedGroupListView(ListView):
+    pass
+
+
+class GroupCreateView(CreateView):
+    pass
+
+
+class GroupDetailView(DetailView):
+    pass
+
+
+class GroupUpdateView(UpdateView):
+    pass
+
+
+class GroupDeleteView(DeleteView):
+    pass
+
+
+class GroupSearchView(View):
+    pass
