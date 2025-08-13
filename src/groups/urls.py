1b6d5f7de3471e89639074a252f01105edd9c396
FRATAK4 <fratak4@gmail.com>
2025-08-13 23:14:21 +0200

    feat(groups app): creates base of urls pathing

--- src/groups/urls.py
+++ src/groups/urls.py
@@ -1,4 +1,21 @@
 from django.urls import path
 
+from .views import (
+    GroupDashboardView,
+    GroupOwnedListView,
+    GroupJoinedListView,
+    GroupCreateView,
+    GroupDetailView,
+    GroupUpdateView,
+    GroupDeleteView,
+)
 
-urlpatterns = []
+urlpatterns = [
+    path("", GroupDashboardView.as_view(), name="group_dashboard"),
+    path("owned/", GroupOwnedListView.as_view(), name="group_owned_list"),
+    path("joined/", GroupJoinedListView.as_view(), name="group_joined_list"),
+    path("create/", GroupCreateView.as_view(), name="group_create"),
+    path("<int:pk>/", GroupDetailView.as_view(), name="group_detail"),
+    path("<int:pk>/edit/", GroupUpdateView.as_view(), name="group_edit"),
+    path("<int:pk>/delete/", GroupDeleteView.as_view(), name="group_delete"),
+]
