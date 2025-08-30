61e1f467ac13cb5c3352fd7062c3e6a12201d869
FRATAK4 <fratak4@gmail.com>
2025-08-30 11:59:49 +0200

    feat(factories): adds create_users command

---
+++ src/accounts/management/commands/create_users.py
@@ -0,0 +1,20 @@
+from django.core.management import BaseCommand
+
+from accounts.factories import UserFactory
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument("count", type=int, default=1)
+
+    def handle(self, *args, **options):
+        count = options["count"]
+
+        self.stdout.write(self.style.WARNING(f"Creating {count} user(s)..."))
+
+        self._create_users(count)
+
+    def _create_users(self, count):
+        users = UserFactory.create_batch(count)
+
+        return users
