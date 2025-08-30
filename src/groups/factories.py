fb01d5835eba288afb7c749fc736f66def900d4d
FRATAK4 <fratak4@gmail.com>
2025-08-30 11:01:05 +0200

    feat(factories): adds GroupMembershipFactory to groups app

--- src/groups/factories.py
+++ src/groups/factories.py
@@ -1,8 +1,9 @@
 from django.utils import timezone
 import factory.django
 from faker import Faker
+import random
 
-from .models import Group, GroupAddRequest
+from .models import Group, GroupAddRequest, GroupMembership
 from ..accounts.factories import UserFactory
 
 fake = Faker()
@@ -40,6 +41,47 @@ class GroupAddRequestFactory(factory.django.DjangoModelFactory):
             case "pending":
                 return None
             case "accepted" | "declined":
+                return timezone.now()
+
+    user = factory.SubFactory(UserFactory)
+    group = factory.SubFactory(GroupFactory)
+
+
+class GroupMembershipFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = GroupMembership
+
+    status = factory.Iterator(["accepted", "kicked", "blocked"])
+
+    @factory.lazy_attribute
+    def started_at(self):
+        match self.status:
+            case "accepted":
+                return timezone.now()
+            case "kicked" | "blocked":
+                return fake.date_between(start_date="-1m", end_date="today")
+
+    @factory.lazy_attribute
+    def kicked_at(self):
+        match self.status:
+            case "accepted":
+                return None
+            case "kicked":
+                return timezone.now()
+            case "blocked":
+                return random.choice(
+                    [
+                        fake.date_between(start_date=self.started_at, end_date="today"),
+                        None,
+                    ]
+                )
+
+    @factory.lazy_attribute
+    def blocked_at(self):
+        match self.status:
+            case "accepted" | "kicked":
+                return None
+            case "blocked":
                 return timezone.now()
 
     user = factory.SubFactory(UserFactory)
