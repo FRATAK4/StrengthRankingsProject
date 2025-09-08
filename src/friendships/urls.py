3f8d348a18d63d193ad358a95779f0fbef26506c
Makaljer <fratak4@gmail.com>
2025-09-08 22:12:43 +0200

    feat(friendships app): adds friend_dashboard url

--- src/friendships/urls.py
+++ src/friendships/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import FriendSearchView, FriendSendRequestView
+from .views import FriendSearchView, FriendSendRequestView, FriendDashboardView
 
 urlpatterns = [
+    path("/", FriendDashboardView.as_view(), name="friend_dashboard"),
     path("search/", FriendSearchView.as_view(), name="friend_search"),
     path(
         "search/<int:pk>", FriendSendRequestView.as_view(), name="friend_send_request"
