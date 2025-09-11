f56d090fc87afa8edae1a4b8b5afc00f86246b36
Makaljer <fratak4@gmail.com>
2025-09-11 23:28:57 +0200

    feat(factories): adds friendships_kick_friends command

---
+++ src/friendships/management/commands/friendships_kick_friends.py
@@ -0,0 +1,26 @@
+import random
+
+from django.core.management import BaseCommand
+from django.db.models import F
+from django.utils import timezone
+
+from friendships.models import Friendship
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        self.stdout.write("Kicking friend(s)...")
+
+        self._kick_friends()
+
+        self.stdout.write("Successfully kicked all friends!")
+
+    def _kick_friends(self):
+        friendships = Friendship.objects.filter(
+            status=Friendship.FriendshipStatus.ACTIVE
+        )
+        for friendship in friendships:
+            friendship.status = (Friendship.FriendshipStatus.KICKED,)
+            friendship.kicked_at = (timezone.now(),)
+            friendship.kicked_by = random.choice([F("user"), F("friend")])
+            friendship.save()
