7ba287fa97b4aff49c474a6347466cb180e1e984
Makaljer <fratak4@gmail.com>
2025-09-13 14:50:21 +0200

    feat(factories): adds friendships_block_users command

---
+++ src/friendships/management/commands/friendships_block_users.py
@@ -0,0 +1,58 @@
+import random
+
+from django.contrib.auth.models import User
+from django.core.management import BaseCommand
+from django.db import transaction
+from django.db.models import Q
+from django.utils import timezone
+
+from friendships.models import Friendship
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument("count", type=int, default=1)
+
+    def handle(self, *args, **options):
+        count = options["count"]
+        users = User.objects.all()
+
+        self.stdout.write(f"Blocking {count} user(s) for each user...")
+
+        for user in users:
+            self._block_users(count, user)
+
+        self.stdout.write(f"Successfully blocked {count} user(s) for each user...")
+
+    @transaction.atomic
+    def _block_users(self, count, user):
+        users_to_block = User.objects.exclude(
+            Q(
+                sent_friendships__friend=user,
+                sent_friendships__status=Friendship.FriendshipStatus.BLOCKED,
+            )
+            | Q(
+                accepted_friendships__user=user,
+                accepted_friendships__status=Friendship.FriendshipStatus.BLOCKED,
+            )
+            | Q(pk=user.pk)
+        )
+        for _ in range(count):
+            user_to_block = random.choice(users_to_block)
+            friendship = Friendship.objects.filter(
+                Q(user=user, friend=user_to_block) | Q(user=user_to_block, friend=user)
+            ).first()
+
+            if not friendship:
+                Friendship.objects.create(
+                    status=Friendship.FriendshipStatus.BLOCKED,
+                    user=user,
+                    friend=user_to_block,
+                    blocked_at=timezone.now(),
+                    blocked_by=user,
+                )
+            else:
+                friendship.status = Friendship.FriendshipStatus.BLOCKED
+                friendship.blocked_at = timezone.now()
+                friendship.blocked_by = user
+                friendship.save()
